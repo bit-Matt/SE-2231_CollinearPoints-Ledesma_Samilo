693586806016f3ee9989a1b31dc2c6f2a4322baf
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-03-01 17:01:38 +0800

    [Adjustment] Separate files & slopeTo implementation

---
+++ src/FastCollinearPoints.ts
@@ -0,0 +1,18 @@
+import Point from "./Point";
+import LineSegment from "./LineSegment";
+
+export default class FastCollinearPoints {
+  constructor(points: Point[]) {
+    // YOUR CODE HERE
+  }
+
+  numberOfSegments(): number {
+    // YOUR CODE HERE
+    return 0;
+  }
+
+  segments(): LineSegment[] {
+    // YOUR CODE HERE
+    return [];
+  }
+}
