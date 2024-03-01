0a664637b9fb0b3b2d23a7b3cda9fb5a317af9e1
bit-Matt <matthew.ledesma-22@cpu.edu.ph>
2024-03-01 15:10:00 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,159 @@
+import p5 from "p5";
+
+const width: number = 800;
+const height: number = 500;
+const padding: number = 50;
+
+let sketch = function (p) {
+  p.setup = function () {
+    p.createCanvas(width, height);
+
+    p.strokeWeight(3);
+    p.stroke("blue");
+
+    // x and y axes
+    p.line(padding, padding, padding, height - padding);
+    p.line(padding, height - padding, width - padding, height - padding);
+
+    // y-axis arrow head
+    p.line(padding, padding, padding - 5, padding + 5);
+    p.line(padding, padding, padding + 5, padding + 5);
+
+    // x-axis arrow head
+    p.line(
+      width - padding,
+      height - padding,
+      width - padding - 5,
+      height - padding + 5
+    );
+    p.line(
+      width - padding,
+      height - padding,
+      width - padding - 5,
+      height - padding - 5
+    );
+
+    p.strokeWeight(0);
+    p.text("(0, 0)", padding + 10, height - 30);
+  };
+
+  class Point {
+    x: number;
+    y: number;
+    p;
+
+    constructor(x: number, y: number) {
+      this.x = x;
+      this.y = y;
+    }
+
+    draw(): void {
+      // DO NOT MODIFY
+
+      p.stroke("black");
+      p.strokeWeight(800);
+      p.point(this.x, this.y);
+    }
+
+    drawTo(that: Point) {
+      // DO NOT MODIFY
+
+      p.stroke("black");
+      p.strokeWeight(200);
+      p.line(this.x, this.y, that.x, that.y);
+    }
+
+    slopeTo(that: Point): number {}
+  }
+
+  class LineSegment {
+    p: Point;
+    q: Point;
+
+    constructor(p: Point, q: Point) {
+      // DO NOT MODIFY
+
+      this.p = p;
+      this.q = q;
+    }
+
+    draw(): void {
+      // DO NOT MODIFY
+
+      p.stroke("black");
+      p.strokeWeight(2);
+      p.line(this.p.x, this.p.y, this.q.x, this.q.y);
+    }
+
+    toString(): string {
+      // DO NOT MODIFY
+
+      return `${this.p} -> ${this.q}`
+    }
+  }
+
+  class BruteCollinearPoints {
+    constructor(points: Point[]) {
+      // YOUR CODE HERE
+    }
+
+    numberOfSegments(): number {
+      // YOUR CODE HERE
+    }
+
+    segments(): LineSegment[] {
+      // YOUR CODE HERE
+    }
+  }
+
+  class FastCollinearPoints {
+    constructor(points: Point[]) {
+      // YOUR CODE HERE
+    }
+
+    numberOfSegments(): number {
+      // YOUR CODE HERE
+    }
+
+    segments(): LineSegment[] {
+      // YOUR CODE HERE
+    }
+  }
+
+  // Declare your point objects here~
+  // const point = new Point(19000, 10000);
+  // const point2 = new Point(10000, 10000);
+
+  // from input6.txt
+  const points: Point[] = [
+    new Point(19000, 10000),
+    new Point(18000, 10000),
+    new Point(32000, 10000),
+    new Point(21000, 10000),
+    new Point(1234, 5678),
+    new Point(14000, 10000),
+  ];
+
+  p.draw = function () {
+    p.translate(padding, height - padding);
+    p.scale(1/100, -1/100);
+
+    // Call your draw and drawTo here.
+
+    // point.draw();
+    // point2.draw();
+    // point.drawTo(point2);
+
+    for (const point of points) {
+      point.draw();
+    }
+
+    const collinear = new FastCollinearPoints(points);
+    for (const segment of collinear.segments()) {
+      console.log(segment.toString());
+      segment.draw();
+    }
+  };
+};
+
+new p5(sketch);
---
+++ src/point.ts
@@ -0,0 +1,13 @@
+class Point {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    draw(): void {
+        
+    }
+}
